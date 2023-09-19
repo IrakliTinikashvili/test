46ee2854885fd7c6d32ba81b8d1645e7c4e71dee
IrakliTinikashvili <iktinikashvili@gmail.com>
2023-09-19 21:36:04 +0400

    my first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+/*
+let x = 100;
+let message = x < 50 && x > 20 ? console.log("x is less than 50 but more than 20") : console.log("Mistake");
+*/
+
+
+/*
+let x = 100;
+
+if(x < 50 && x > 20){
+    console.log("x is less than 50 but more than 20")
+}
+else{
+    console.log("Mistake")
+}
+*/
+
+
+/*
+let firstName = "Mariam";
+
+
+if(firstName === "Mariam") {
+    console.log(true)
+}
+else{
+    console.log(false)
+}
+*/
+
+
+/*
+let firstName = "Mariam";
+
+switch (firstName) {
+    case "Mariami":
+        console.log(true);
+        break;
+    default: console.log(false);
+}
+*/
+
+
+/*
+let firstName = "Irakli";
+console.log(typeof firstName);
+*/
+
+
+/*
+let x = 10;
+console.log(x.toString());
+*/
